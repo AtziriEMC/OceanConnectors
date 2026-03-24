999b6bf250fd683cd1005a244612e5a4aa103107
Andy <andyduranmartinez@gmail.com>
2026-03-23 21:55:25 -0600

    cambio contenido en app

--- OceanConnectors/src/App.jsx
+++ OceanConnectors/src/App.jsx
@@ -1,120 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Contacto from './components/Contacto';
+import InicioSesion from './components/InicioSesion';
+import MenuEstaciones from './components/MenuEstaciones';
+import Mision from './components/Mision';
+import Registro from './components/registro';
+import Tabla from './components/tabla';
+import Totales from './components/Totales';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [vista, setVista] = useState('inicio');
+
+  // Función para cambiar de vista y cerrar el menú al mismo tiempo
+  const cambiarVista = (nuevaVista) => {
+    setVista(nuevaVista);
+    const menuToggle = document.getElementById('menu-toggle');
+    if (menuToggle) menuToggle.checked = false; // Esto cierra el menú
+  };
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className={vista === 'inicio' ? 'layout-inicio' : 'layout-app'}>
+      
+      {/* header: Siempre fijo arriba */}
+      <nav className="contenedorsup">
+        <img 
+          src="/imagenes/logo.png" 
+          alt="logo" 
+          className="logo-img" 
+          onClick={() => setVista('inicio')} 
+        />
+        <input type="checkbox" id="menu-toggle" style={{ display: 'none' }} />
+        <label for="menu-toggle" class="menu-icon">☰</label>
+        <ul className="menu">
+          <li><button onClick={() => cambiarVista('registro')} className="btn-nav">REGISTRO</button></li>
+          <li><button onClick={() => cambiarVista('mision')} className="btn-nav">MISIÓN</button></li>
+          <li><button onClick={() => cambiarVista('contacto')} className="btn-nav">CONTACTO</button></li>
+          <li><button onClick={() => cambiarVista('totales')} className="btn-nav">GRAFICAS</button></li>
+          <li><button onClick={() => cambiarVista('iniciosesion')} className="btn-nav">INICIAR SESION</button></li>
+        </ul>
+      </nav>
 
-      <div className="ticks"></div>
+      {/* contenido, Aquí es donde se centra la tarjeta */}
+      <main className="seccion-principal">
+        {vista === 'inicio' && (
+          <div className="home-content">
+            <h1 className="titulo-principal">
+              HAGAMOS CIENCIA CON LA BASURA<br /> EN LAS PLAYAS DE MÉXICO 
+            </h1>
+            <p className="descripcion">
+              Registra residuos encontrados y contribuye al análisis<br /> 
+              ambiental para mejorar nuestras playas 
+            </p>
+            <div className="seccion-registro">
+              <button className="btn-verde" onClick={() => setVista('registro')}> 
+                NUEVO REGISTRO 
+              </button>
+            </div>
+          </div>
+        )}
+        {vista === 'mision' && <Mision alVolver={() => setVista('inicio')} />}
+        {vista === 'contacto' && <Contacto alVolver={() => setVista('inicio')} />}
+        {vista === 'totales' && <Totales alVolver={() => setVista('inicio')} />}
+        {vista === 'registro' && <Registro alVolver={() => setVista('inicio')} alSiguiente={() => setVista('menuEstaciones')} />}
+        {vista === 'iniciosesion' && <InicioSesion alVolver={() => setVista('inicio')} />}
+        {vista === 'tabla' && <Tabla alVolver={() => setVista('menuEstaciones')} alSiguiente={() => setVista('menuEstaciones')} />}
+        {/* Configuración del Menú de Estaciones */}
+{vista === 'menuEstaciones' && (
+  <MenuEstaciones 
+    alVolver={() => setVista('registro')} 
+    alEstacion1={() => setVista('estacion1')} 
+    alEstacion2={() => setVista('estacion2')} 
+    alEstacion3={() => setVista('estacion3')} 
+  />
+)}
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+{/* Estación 1 */}
+{vista === 'estacion1' && (
+  <Tabla 
+    titulo="ESTACIÓN 1" 
+    storageKey="datosEstacion1" 
+    alVolver={() => setVista('menuEstaciones')} 
+    alSiguiente={() => setVista('menuEstaciones')} 
+  />
+)}
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+{/* Estación 2 */}
+{vista === 'estacion2' && (
+  <Tabla 
+    titulo="ESTACIÓN 2" 
+    storageKey="datosEstacion2" 
+    alVolver={() => setVista('menuEstaciones')} 
+    alSiguiente={() => setVista('menuEstaciones')} 
+  />
+)}
+
+{/* Estación 3 */}
+{vista === 'estacion3' && (
+  <Tabla 
+    titulo="ESTACIÓN 3" 
+    storageKey="datosEstacion3" 
+    alVolver={() => setVista('menuEstaciones')} 
+    alSiguiente={() => setVista('menuEstaciones')} 
+  />
+)}
+      </main>
+    </div>
   )
 }
 
